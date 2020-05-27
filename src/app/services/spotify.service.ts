b4cdc64ce259533ac8d1fcf17b9b15f0e50f4828
miguel_casas <miguel.casas@atmira.com>
2020-05-27 18:14:33 +0200

    servicios novedades y buscar artista

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -13,13 +13,20 @@ export class SpotifyService {
   getNewReleases(){
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQC6jmI7X5GwuTlfVe6LvfDeG7R0lEFyV_4g3wRmxrQgMLR6qgN6pxTW_hQCo5YJ_a5gIlCi0P4dHTSGNJQ'
+      'Authorization': 'Bearer BQD5FeJ32uZjts1tXbyoZ-48v7htu-fovjn0BRko3WUTmaqzsjo52maJgU7oC1Fz5e2O2Iq0gA4S-ixkibQ'
     });
 
-    this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
-          .subscribe( data => {
-            console.log(data);
-          } );
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
+
+  }
+
+  getArtista( termino: string) {
+   
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQD5FeJ32uZjts1tXbyoZ-48v7htu-fovjn0BRko3WUTmaqzsjo52maJgU7oC1Fz5e2O2Iq0gA4S-ixkibQ'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers });
 
   }
 
