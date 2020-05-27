5a61c8f1438ae2bdc3c9101ee7d79fb4ebbc994b
miguel_casas <miguel.casas@atmira.com>
2020-05-27 18:31:05 +0200

    uso de la función map para obtener del servicio solo los datos que me interesan

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+// Reactive extensions
+import { map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -16,7 +19,8 @@ export class SpotifyService {
       'Authorization': 'Bearer BQD5FeJ32uZjts1tXbyoZ-48v7htu-fovjn0BRko3WUTmaqzsjo52maJgU7oC1Fz5e2O2Iq0gA4S-ixkibQ'
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
+              .pipe( map( data => data['albums'].items));
 
   }
 
@@ -26,7 +30,8 @@ export class SpotifyService {
       'Authorization': 'Bearer BQD5FeJ32uZjts1tXbyoZ-48v7htu-fovjn0BRko3WUTmaqzsjo52maJgU7oC1Fz5e2O2Iq0gA4S-ixkibQ'
     });
 
-    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers });
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers })
+              .pipe( map( data => data['artists'].items));
 
   }
 
